d43d5c833101a36e54201da1293d73ff379da81d
Toprak Daldal <toprakdaldal@unknown5e16399f339d.home>
2026-05-02 19:57:04 +0300

    Unity project update

--- Assets/oyuncuKontrol.cs
+++ Assets/oyuncuKontrol.cs
@@ -6,11 +6,15 @@ public class oyuncuKontrol : MonoBehaviour
     public float speed=0.0f;
     public Rigidbody2D rigidbody2D;
     private Animator animator;
+    private Vector3 charPos;
+    [SerializeField] GameObject _camera;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidbody2D=GetComponent<Rigidbody2D>();
         animator=GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -27,6 +31,13 @@ public class oyuncuKontrol : MonoBehaviour
             Debug.Log(message: "hiz 0.0");
         }
         animator.SetFloat(name:"speed",speed);
-        rigidbody2D.linearVelocity=new Vector2(x:speed,y:0f);
+        //rigidbody2D.linearVelocity=new Vector2(x:speed,y:0f);
+        Vector3 charPos = transform.position;
+        charPos=new Vector3(x:charPos.x+(speed*Time.deltaTime),charPos.y,charPos.z);
+        transform.position=charPos;
+    }
+    private void LateUpdate()
+    {
+        _camera.transform.position=new Vector3(charPos.x,charPos.y,charPos.z-1.0f);
     }
 }
